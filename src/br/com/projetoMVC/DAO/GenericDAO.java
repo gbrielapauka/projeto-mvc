4f535bbf9de3ffb21e06395a62143066e890673a
Gabriela Batista Pauka <gabrielabpauka@hotmail.com>
2024-04-11 20:21:25 -0300

    feat(dao): Declarando interface GenericDAO

---
+++ src/br/com/projetoMVC/DAO/GenericDAO.java
@@ -0,0 +1,16 @@
+package br.com.projetoMVC.DAO;
+
+import java.util.List;
+
+public interface GenericDAO {
+	
+	public List<Object> listarTodos();
+	
+	public Object listarPorId(int id);
+	
+	public Boolean cadastrar(Object object);
+	
+	public Boolean alterar(Object object);
+	
+	public void excluir(int id);
+}
